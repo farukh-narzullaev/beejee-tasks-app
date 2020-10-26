6ab7b9f5ff7444afdbe70444e8ab5c4e5ff0d918
Faruh Narzullaev <faruh.narzullaev@sibers.com>
2020-10-26 15:20:56 +0600

    Add ENV vars

--- public/index.php
+++ public/index.php
@@ -1,13 +1,5 @@
 <?php
 
-use PDO;
-
-$dbh = new PDO('mysql:host=us-cdbr-east-02.cleardb.com;dbname=heroku_4dfc0b5377f7dad', 'b6dde05496a580', 'fe2b10ce');
-
-var_dump($dbh);
-die;
-
-
 session_start();
 
 require_once '../vendor/autoload.php';
